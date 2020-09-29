8425d55a13369634717e72ee26e15cae28a5feee
Ammon Jeffery <savingphace@gmail.com>
2020-09-29 00:06:19 -0600

    finished fetch request

--- public/service-worker.js
+++ public/service-worker.js
@@ -42,3 +42,11 @@ self.addEventListener("install", function(evt) {
   
       return;
     }
+    evt.respondWith(
+        caches.open(CACHE_NAME).then(cache => {
+          return cache.match(evt.request).then(response => {
+            return response || fetch(evt.request);
+          });
+        })
+      );
+    });
