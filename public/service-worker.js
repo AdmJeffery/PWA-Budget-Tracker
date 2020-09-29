9a096e480b930306e6314d4b4e13965c18e3602f
Ammon Jeffery <savingphace@gmail.com>
2020-09-28 23:56:09 -0600

    added pages to FILES TO CACHE

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,10 @@
 const FILES_TO_CACHE = [
-    
-  ];
+    "/",
+    "/index.html",
+    "/styles.css",
+    "/index.js",
+    "/manifest.json",
+   ]; 
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
